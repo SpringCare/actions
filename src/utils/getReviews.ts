f125eb5127bd99f2bda445a0321310c8bae8d3ac
kassanj <kassandram022@gmail.com>
2020-03-09 10:13:17 -0700

    add try/catch

--- src/utils/getReviews.ts
+++ src/utils/getReviews.ts
@@ -2,14 +2,19 @@
 const github = require('@actions/github');
 
 export async function getReviews(token, pullNumber): Promise<{}> {
+	
+	try {
+		const client = new github.GitHub(token);
 
-	const client = new github.GitHub(token);
+		return await client.pulls.listReviews({
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			// eslint-disable-next-line @typescript-eslint/camelcase
+			pull_number: pullNumber,
+		});
 
-	return await client.pulls.listReviews({
-		owner: github.context.repo.owner,
-		repo: github.context.repo.repo,
-		// eslint-disable-next-line @typescript-eslint/camelcase
-		pull_number: pullNumber,
-	});
+	} catch(error) {
+		console.log(error);
+	}
 }
 
