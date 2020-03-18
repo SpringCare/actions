d1c08e1a959cf4acf3cc79c325595501acf863db
kassanj <kassandram022@gmail.com>
2020-03-18 11:47:13 -0700

    test regex

--- dist/on_merge/index.js
+++ dist/on_merge/index.js
@@ -2499,17 +2499,27 @@ function parseOptions(options, log, hook) {
 const github = __webpack_require__(469);
 function main() {
     const targetBranch = github.context.ref;
-    const description = github.context.payload.pull_request.body;
+    const text = github.context.payload.pull_request.body;
     console.log('target branch: ', targetBranch);
-    console.log('body text: ', description);
+    console.log('body text: ', text);
+    const urlRegex = /(https?:\/\/pivotaltracker[^\s]+)/g;
+    const urls = text.match(urlRegex);
+    console.log(urls);
+    // pivotalTrackerCall(url);
 }
 // Call the main function.
 main();
+// function pivotalTrackerCall(url) {
+// 	console.log(url);
+// }
 // 1. Get target branch (staging || master)
 // 2. Determine target PT workflow change (finish || deliver)
 // 3. Parse link from PR body (PT Ticket ###)
 // 4. Setup PT Connection
 // a. Update PT Ticket ### with new workflow (finish || deliver)
+// PUT /projects/2428649/stories/{story_id}
+// current_state -> accepted, delivered, finished
+// axios
 
 
 /***/ }),
--- src/on_merge/index.ts
+++ src/on_merge/index.ts
@@ -2,14 +2,29 @@ const github = require('@actions/github');
 
 function main(): void {
 	const targetBranch = github.context.ref;
-	const description = github.context.payload.pull_request.body;
+	const text = github.context.payload.pull_request.body;
 
 	console.log('target branch: ', targetBranch);
-	console.log('body text: ', description);
+	console.log('body text: ', text);
+
+
+	const urlRegex = /(https?:\/\/pivotaltracker[^\s]+)/g;
+	const urls = text.match(urlRegex);
+
+	console.log(urls);
+
+
+	// pivotalTrackerCall(url);
+
 }
 
 // Call the main function.
 main();
+
+
+// function pivotalTrackerCall(url) {
+// 	console.log(url);
+// }
 
 
 // 1. Get target branch (staging || master)
@@ -17,3 +32,9 @@ main();
 // 3. Parse link from PR body (PT Ticket ###)
 // 4. Setup PT Connection
 // a. Update PT Ticket ### with new workflow (finish || deliver)
+
+
+
+// PUT /projects/2428649/stories/{story_id}
+// current_state -> accepted, delivered, finished
+// axios
