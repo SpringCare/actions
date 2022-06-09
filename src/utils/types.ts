d9d53d49bf385dc427e4d981d9c2c6d789e0af60
Chrishanth Rogers <chrishanthmathew91@gmail.com>
2022-06-09 19:38:33 +0530

    Add: Fixes for staging label not showing up

---
+++ src/utils/types.ts
@@ -0,0 +1,50 @@
+export interface BranchLabelerInputs {
+    token: string;
+	branch: string;
+	label: string;
+	color: string;
+}
+
+export interface PrLabelerInputs extends BranchLabelerInputs {
+	requiredReviews: number;
+	labelWIP: boolean;
+}
+
+export interface Client {
+    issues: any;
+}
+
+interface Head {
+	sha: string;
+}
+
+export interface Label {
+	name: string;
+}
+export interface Pr {
+	number: number;
+	title: string;
+	head: Head;
+	commits_url: string;
+	labels: Array<Label>;
+    draft: boolean;
+}
+
+export interface Repository {
+    owner: string;
+    repo: string;
+}
+
+interface Author {
+    name: string;
+}
+
+interface CommitMetaData {
+    author: Author;
+}
+
+export interface Commit {
+    sha: string;
+    commit: CommitMetaData;
+    parents: Array<Commit>;
+}
