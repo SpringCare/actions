f568494dfd7241f68f4411ec48f0e7f1fb5f54df
Utsav Parmar <uparmar@springhealth.com>
2022-07-05 11:37:56 +0530

    Add: Branch filter

--- src/create_crowdin_task/index.ts
+++ src/create_crowdin_task/index.ts
@@ -1,32 +1,67 @@
-import {ProjectsGroups, SourceFiles} from '@crowdin/crowdin-api-client';
+import {ProjectsGroups, SourceFiles, Tasks} from '@crowdin/crowdin-api-client';
 
 const core = require('@actions/core');
 const github = require('@actions/github');
 const crowdin = require('@crowdin/crowdin-api-client');
 
 import { Octokit } from '@octokit/core';
+import { addLabels } from '../utils/labeler';
 
 async function getProjectId(projectsGroupsApi: ProjectsGroups): Promise<number> {
 	const response = await projectsGroupsApi.listProjects();
 	return response.data[0].data.id;
 }
 
-async function getEnDirectoryId(projectId: number, sourceFilesApi: SourceFiles): Promise<number> {
+async function getEnDirectoryId(sourceFilesApi: SourceFiles, projectId: number, branchId: number): Promise<number> {
 	console.log(projectId);
 	const cherrimResponse = await sourceFilesApi.listProjectDirectories(projectId, { filter: 'cherrim' });
-	const cherrimDirectoryId = cherrimResponse.data[0].data.id;
+	const cherrimDirectoryId = cherrimResponse.data[0].data.id; // Todo: won't work cause the first may not necessarily belong to the branch we want
 
 	const enResponse = await sourceFilesApi.listProjectDirectories(projectId, {
-		directoryId: cherrimDirectoryId, filter: 'en', recursion: 'true'
+		directoryId: cherrimDirectoryId, branchId: branchId, filter: 'en', recursion: 'true' // altho cherrimDirectoryId is overwritten by branchId filter
 	});
 	return enResponse.data[0].data.id;
+}
+
+async function getBranchId(sourceFilesApi: SourceFiles, projectId: number, branchName: string): Promise<number> {
+	const branches = await sourceFilesApi.listProjectBranches(projectId, {
+		name: branchName
+	});
+
+	return branches.data[0].data.id;
+}
+
+async function getFileIds(sourceFilesApi: SourceFiles, projectId: number, enLocaleDirId: number): Promise<Array<number>> {
+	const files = await sourceFilesApi.listProjectFiles(projectId, {
+		directoryId: enLocaleDirId
+	});
+
+	return files.data.map(elem => elem.data.id);
+}
+
+async function createTask(tasksApi: Tasks, projectId: number, filesIds: Array<number>, languages: string[]): Promise<void> {
+	try {
+		for (const lang of languages) {
+			await tasksApi.addTask(projectId, {
+				title      : 'SH Internal Task',
+				type       : 2,
+				fileIds    : filesIds,
+				languageId : lang,
+				vendor     : 'oht'
+			});
+		}
+	} catch (e) { // Todo: Check for specific error - Task not created
+		throw 'Manual Translation Needed!';
+	}
 }
 
 async function main (): Promise<void> {
 	const inputs: {
 		token: string;
+		branch: string;
 	} = {
-		token         : core.getInput('repo-token', { required: true }),
+		token  : core.getInput('repo-token', { required: true }),
+		branch : core.getInput('branch'),
 	};
 
 	const octokit = new Octokit({ auth: inputs.token });
@@ -38,29 +73,28 @@ async function main (): Promise<void> {
 		tasksApi
 	} = new crowdin.default({ token });
 
+	const branchName = '[SpringCare.arceus] ' + inputs.branch.replace('/', '.');
 	const projectId = await getProjectId(projectsGroupsApi);
-	const enLocaleDirId = await getEnDirectoryId(projectId, sourceFilesApi);
-
-	const filesResponse = await sourceFilesApi.listProjectFiles(projectId, {
-		directoryId: enLocaleDirId
-	});
+	const branchId = await getBranchId(sourceFilesApi, projectId, branchName);
+	const enLocaleDirId = await getEnDirectoryId(sourceFilesApi, projectId, branchId);
 
 	// Todo: get changed files
-	// extract file ids from files
 	// What if?
 	//	- output changed files from translation diff action using `setOutput`
 	//	- filter out only the changed files
+	const filesIds = await getFileIds(sourceFilesApi, projectId, enLocaleDirId);
 
-	// Todo: create task for changed files
-	// check with updatedAt
+	// Todo: get languages - listProjects() lists all language ids - targetLanguageIds
+	const languages = [];
 
-	// loop
-	const response = await tasksApi.addTask(projectId, {
-		fileIds: filesIds,
-		languageId: 'fr',
-		type: 0,
-		title: 'SH Internal Task',
-	});
+	const pullNumber = github.context.payload.pull_request.number;
+	const client = new github.GitHub(inputs.token);
+	try {
+		await createTask(tasksApi, projectId, filesIds, languages);
+		await addLabels(client, pullNumber, ['Translations In Progress']);
+	} catch (e) {
+		await addLabels(client, pullNumber, ['Manual Translations Needed']);
+	}
 }
 
 main();
