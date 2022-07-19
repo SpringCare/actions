6cd3d7de5bfe6f4b67f2ca8257a1daa9baafae7e
Utsav Parmar <uparmar@springhealth.com>
2022-07-19 18:14:30 +0530

    TST: sleep

--- src/create_crowdin_task/index.ts
+++ src/create_crowdin_task/index.ts
@@ -59,11 +59,38 @@ async function getTargetLanguages(projectsGroupsApi: ProjectsGroups): Promise<Ar
 	return response.data[0].data.targetLanguageIds;
 }
 
+function sleep(ms: number): Promise<unknown> {
+	return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+const doMainStuff = async (inputs: { token: string; branch: string }, projectsGroupsApi: ProjectsGroups, sourceFilesApi: SourceFiles, tasksApi: Tasks, retry: number): Promise<number> => {
+	const branchName = '[SpringCare.arceus] ' + inputs.branch.replace('/', '.');
+	const projectId = await getProjectId(projectsGroupsApi);
+	const branchId = await getBranchId(sourceFilesApi, projectId, branchName);
+	const enLocaleDirId = await getEnDirectoryId(sourceFilesApi, projectId, branchId);
+
+	// Todo: get changed files
+	// What if?
+	//	- output changed files from translation diff action using `setOutput`
+	//	- filter out only the changed files
+	const filesIds = await getFileIds(sourceFilesApi, projectId, enLocaleDirId);
+
+	const languages = await getTargetLanguages(projectsGroupsApi);
+
+	try {
+		await createTask(tasksApi, projectId, filesIds, languages);
+		retry = 0;
+	} catch (e) {
+		if (e.message === 'Language has no untranslated words')
+			retry -= 1;
+	}
+	return retry;
+};
+
 async function main (): Promise<void> {
 	let retry = 1;
 
-	const intervalId = setInterval(async () => {
-		const inputs: {
+	const inputs: {
 				token: string;
 				branch: string;
 			} = {
@@ -71,45 +98,21 @@ async function main (): Promise<void> {
 				branch : core.getInput('branch'),
 			};
 
-		const octokit = new Octokit({auth: inputs.token});
+	// Todo: get the token from the env variable
+	const token = 'cdb855490129eee0b5807de981ebc4b22b8280ccca2bea52f544af17d52adde2f4cccdb25b9d5272';
+	const {
+		sourceFilesApi,
+		projectsGroupsApi,
+		tasksApi
+	} = new crowdin.default({token});
 
-		// Todo: get the token from the env variable
-		const token = 'cdb855490129eee0b5807de981ebc4b22b8280ccca2bea52f544af17d52adde2f4cccdb25b9d5272';
-		const {
-			sourceFilesApi,
-			projectsGroupsApi,
-			tasksApi
-		} = new crowdin.default({token});
-
-		const branchName = '[SpringCare.arceus] ' + inputs.branch.replace('/', '.');
-		const projectId = await getProjectId(projectsGroupsApi);
-		const branchId = await getBranchId(sourceFilesApi, projectId, branchName);
-		const enLocaleDirId = await getEnDirectoryId(sourceFilesApi, projectId, branchId);
-
-		// Todo: get changed files
-		// What if?
-		//	- output changed files from translation diff action using `setOutput`
-		//	- filter out only the changed files
-		const filesIds = await getFileIds(sourceFilesApi, projectId, enLocaleDirId);
-
-		const languages = await getTargetLanguages(projectsGroupsApi);
-
-		const pullNumber = github.context.payload.pull_request.number;
-		const client = new github.GitHub(inputs.token);
-		try {
-			await createTask(tasksApi, projectId, filesIds, languages);
-			retry = 0;
-		} catch (e) {
-			if (e.message === 'Language has no untranslated words')
-				retry -= 1;
+	// eslint-disable-next-line no-constant-condition
+	while (true) {
+		retry = await doMainStuff(inputs, projectsGroupsApi, sourceFilesApi, tasksApi, retry);
+		if (retry > 0) {
+			await sleep(2 * 60 * 1000);
 		}
-	}, 2 * 60 * 1000); // sync time
-
-	setInterval ( () => {
-		if (retry === 0)
-			clearInterval(intervalId);
-	}, 2 * 60 * 1000);
-
+	}
 }
 
 main();
