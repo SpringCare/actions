e9020f94e75423f2c0b64ae94e10873ce1c40066
Leo Reading <leo.reading@springhealth.com>
2025-02-24 14:52:15 -0500

    Adding debugging logs

--- src/utils/getReviews.ts
+++ src/utils/getReviews.ts
@@ -2,11 +2,18 @@ const github = require('@actions/github');
 
 export async function getReviews(token: string, pullNumber: string): Promise<{data: []}> {
 
+  console.log('Getting reviews for PR', pullNumber);
 	const client = new github.GitHub(token);
 
-	return await client.pulls.listReviews({
-		owner       : github.context.repo.owner,
-		repo        : github.context.repo.repo,
-		pull_number : pullNumber,
-	});
+  try {
+    return await client.pulls.listReviews({
+      owner       : github.context.repo.owner,
+      repo        : github.context.repo.repo,
+      pull_number : pullNumber,
+    });
+  } catch (error) {
+    console.error('Error getting reviews');
+    console.error(error);
+    return { data: [] };
+  }
 }
